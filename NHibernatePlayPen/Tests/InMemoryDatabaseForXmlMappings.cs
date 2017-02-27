d57334b585d62f71f2f90190434d2fd9842dee58
Murali Poola <murali.poola@campsystems.com>
2017-02-27 13:35:30 +0530

    Table per subclass mapping

--- NHibernatePlayPen/Tests/InMemoryDatabaseForXmlMappings.cs
+++ NHibernatePlayPen/Tests/InMemoryDatabaseForXmlMappings.cs
@@ -22,7 +22,8 @@ namespace NHibernatePlayPen.Tests
                 .SetProperty(Environment.ConnectionDriver, typeof (SQLite20Driver).AssemblyQualifiedName)
                 .SetProperty(Environment.ConnectionString, "data source =:memory:")
                 .AddFile(@"K:\GitHub\PlayPens\NHibernatePlayPen\Persistence\Mappings\Xml\Employee.hbm.xml")
-                .AddFile(@"K:\GitHub\PlayPens\NHibernatePlayPen\Persistence\Mappings\Xml\Benefit.hierarchy.hbm.xml");
+                //.AddFile(@"K:\GitHub\PlayPens\NHibernatePlayPen\Persistence\Mappings\Xml\Benefit.hierarchy.hbm.xml");
+                .AddFile(@"K:\GitHub\PlayPens\NHibernatePlayPen\Persistence\Mappings\Xml\Benefit.subclass.hbm.xml");
 
             SessionFactory = Config.BuildSessionFactory();
             Session = SessionFactory.OpenSession();
